da6a3d7652525e293accd4652494e3a09a0b5a05
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-08-21 20:18:52 -0700

    Business logic complete. Gotta be an easier way to do it, though. Seems unweildly in it's current state. TODO: Write front-end JS/CSS.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,13 +2,36 @@ const express = require('express');
 const friends = require('../data/friends');
 const router = express.Router();
 
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
 router.route('/friends')
-.get( (req, res) => {
-    res.json(friends);
-})
-.post((req, res) => {
-    //TODO BIDNESS LOGIC
-})
+    .get((req, res) => {
+        res.json(friends);
+    })
+    .post((req, res) => {
+        const user = req.body;
+        let totalDifference = Number.MAX_SAFE_INTEGER - 1;
+        let index;
+
+        for (let i = 0; i < friends.length; i++) {
+            let newDifference = 0;
+            let scores = friends[i].scores;
+
+            for (let j = 0; j < scores.length; j++) {
+                newDifference += Math.abs(user.scores[j] - scores[j]);
+            }
+
+
+            if (newDifference < totalDifference) {
+                totalDifference = newDifference;
+                index = i;
+            }
+
+            newDifference = 0;
+        }
+
+        res.json(friends[index]);
+    })
 
 module.exports = router;
