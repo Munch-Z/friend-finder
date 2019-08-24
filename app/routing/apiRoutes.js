889407a3703bb823640b8b3094b3405c26ecd505
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-08-23 17:37:36 -0700

    Reformatted business functionality for clarity.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,33 +5,41 @@ const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
+function findFriend(user) {
+    let totalDifference = Number.MAX_SAFE_INTEGER - 1;
+    let index;
+    
+    for (let i = 0; i < friends.length; i++) {
+        let newDifference = 0;
+        let scores = friends[i].scores;
+    
+        for (let j = 0; j < scores.length; j++) {
+            newDifference += Math.abs(user.scores[j] - scores[j]);
+        }
+    
+    
+        if (newDifference < totalDifference) {
+            totalDifference = newDifference;
+            index = i;
+        }
+    
+        newDifference = 0;
+    }
+
+    return index;
+}
+
+
 router.route('/friends')
     .get((req, res) => {
         res.json(friends);
     })
     .post((req, res) => {
         const user = req.body;
-        let totalDifference = Number.MAX_SAFE_INTEGER - 1;
-        let index;
 
-        for (let i = 0; i < friends.length; i++) {
-            let newDifference = 0;
-            let scores = friends[i].scores;
+        friends.push(user);
 
-            for (let j = 0; j < scores.length; j++) {
-                newDifference += Math.abs(user.scores[j] - scores[j]);
-            }
-
-
-            if (newDifference < totalDifference) {
-                totalDifference = newDifference;
-                index = i;
-            }
-
-            newDifference = 0;
-        }
-
-        res.json(friends[index]);
+        res.json(friends[findFriend(user)]);
     })
 
 module.exports = router;
