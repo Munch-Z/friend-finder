e5a576a97f074853d4296acaab7c125fca764e4e
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-08-24 09:32:37 -0700

    Adjusted routing for Heroku Support

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,12 @@ router.get('/survey', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
 })
 
-router.get('/home', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
 })
 
 router.get('*', (req, res) => {
-    res.redirect('/home');
+    res.redirect('/');
 })
 
 
